37002bb3991eb5b44add85e2bfc722e8b848cf5f
Bill JINLAPAT <52265259+retore123@users.noreply.github.com>
2019-09-14 14:25:28 +0700

    Add files via upload

---
+++ Assignment #9/console/migrations/m190912_082535_create_subject_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%subject}}`.
+ */
+class m190912_082535_create_subject_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%subject}}', 
+        [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'section' => $this->string(),
+            'teac_id' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%subject}}');
+    }
+}
